0f2264fdbb86c961765da1b4a37e46d2f2eb1500
LaraViera <32910017+LaraViera@users.noreply.github.com>
2018-01-09 18:24:11 +0000

    Primera Versión Kata 5

---
+++ src/kata5/Kata5.java
@@ -0,0 +1,35 @@
+package kata5;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Kata5 {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, FileNotFoundException, IOException {
+        // TODO code application logic here
+        Class.forName("org.sqlite.JDBC");
+        Connection con = DriverManager.getConnection("jdbc:sqlite:.\\kata5.db");
+        
+        Statement st = con.createStatement();
+        
+        String query = "Select * from People";
+        ResultSet rs = st.executeQuery(query);
+        
+        while(rs.next()){
+            System.out.print(rs.getInt(1)+ " ");
+            System.out.print(rs.getString(2)+ " ");
+            System.out.print(rs.getString(3)+ " ");
+            System.out.println(rs.getString(4));
+        }
+        
+    }
+    
+}
